44bdffe86fd68a5161db9490413ba8d2944dba53
Hudso278 <Hudso278@umn.edu>
2018-06-14 15:53:38 -0500

    New Repo

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Jimi Hendrix',
+		photo: 'http://www.gstatic.com/tv/thumb/persons/31690/31690_v9_ba.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chris Cornell',
+		photo: 'https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/11/chris_cornell.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Prince',
+		photo: 'https://media.npr.org/assets/img/2017/04/28/prince-purple-rain_wide-9e4c5c92b0580c5b17b5fbf6b156f618c3144294-s900-c85.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Kurt Cobain',
+		photo: 'http://entertainment.ie//images_content/rectangle/620x372/kurt-cobain-1024.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jon Bonham',
+		photo: 'https://mm.aiircdn.com/5/59de6fea3bd8d.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Chester Bennington',
+		photo: 'https://healthyceleb.com/wp-content/uploads/2017/07/Chester-Bennington-Warner-Music-Group-GRAMMY-Party-February-2017.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dimebag Darrel',
+		photo: 'https://crypticrock.com/wp-content/uploads/2017/11/Dimevision_promo_photo.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Michael Jackson',
+		photo: 'https://www.billboard.com/files/media/06-Michael-Jackson-life-portrait-a-billboard-1548.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cliff Burton',
+		photo: 'http://images.equipboard.com/uploads/user/image/9819/big_cliff-burton.jpg?v=1528916179',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Malcolm Young',
+		photo: 'https://static01.nyt.com/images/2017/11/19/obituaries/19xp-young-sub1/merlin_130260845_88bc646d-f0e7-4da4-b3c4-12c937ce0f02-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
